9970c303f81bea898623958017c906ed653e45d0
Ian Buffington <ian.buffington@gmail.com>
2026-06-20 14:42:17 +0000

    Match GitHub link style to home page footer

--- site/sw.js
+++ site/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'password-gen-v2';
+const CACHE = 'password-gen-v3';
 const ASSETS = ['/', '/style.css', '/app.js', '/manifest.json', '/icons/icon-192.svg', '/icons/icon-512.svg'];
 
 self.addEventListener('install', (e) => {
